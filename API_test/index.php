c203c3fd7966bfe542e9f616de2277a5aa19db57
Leonid <scorpion23997@gmail.com>
2021-01-14 20:11:51 +0200

    create client part

--- API_test/index.php
+++ API_test/index.php
@@ -31,13 +31,7 @@ require "functions.php";
 
     <div class="row row-cols-1 row-cols-md-3">
 
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Название карточки</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Манящая кнопка</a>
-            </div>
-        </div>
+
 
     </div>
 
--- API_test/main.js
+++ API_test/main.js
@@ -2,5 +2,16 @@ async function getPosts(){
     let res = await fetch('https://jsonplaceholder.typicode.com/posts')     //показываю откуда  получаю данные
     let posts = await res.json()        //получаю данные в json
     console.log(posts)
+
+    posts.forEach((post)=> {
+        document.querySelector('.row-cols-md-3').innerHTML += `
+        <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">${post.body}</p>
+                <a href="#" class="btn btn-primary">Манящая кнопка</a>
+            </div>
+        </div>`
+    })
 }
 getPosts()
