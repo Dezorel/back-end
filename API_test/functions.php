7e74b995e118dd10db3e402ea1880a9eee899640
Leonid <scorpion23997@gmail.com>
2021-01-14 21:28:24 +0200

    create answer server 404 and not found status

--- API_test/functions.php
+++ API_test/functions.php
@@ -5,12 +5,25 @@ function getPosts(){
     global $link;
     $sql='SELECT * FROM `myblog`';
     $query = $link->query($sql);
-    return $query->fetchAll();
+    $data = $query->fetchAll();
+    echo json_encode($data);   //делаю массив в json
 }
 function getPost($id){
     global $link;
     $sql="SELECT * FROM `myblog` WHERE `id_post` = '$id'";
     $query = $link->query($sql);
-    return $query->fetchAll();
+    $data = $query->fetchAll();
+    if(!$data){        // если пост не найден
+        $res = [        //устанавливаю ответ если поста нет
+            "status"=>false,
+            "message"=>"Post not found"
+        ];
+        http_response_code(404);   //указываем код который возвращаем
+        echo json_encode($res);
+    }
+    else {
+
+        echo json_encode($data);
+    }
 }
 
