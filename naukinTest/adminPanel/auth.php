8d5348c979502ad817f1a24a6e6ae2b259082709
Leonid <scorpion23997@gmail.com>
2021-03-16 22:12:18 +0200

    create authorization

--- naukinTest/adminPanel/auth.php
+++ naukinTest/adminPanel/auth.php
@@ -1,6 +1,30 @@
 <?php
+session_start();
 
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-var_dump($login .' '.$password);
+if($login === "NaukinAdminPanel"){
+    if($password == "123"){
+        $_SESSION['user']=[
+            "status"=>'true',
+        ];
+        header('Location: admin.php');
+        exit();
+    }
+    else{
+        $_SESSION['user']=[
+            "status"=>'false',
+        ];
+        header('Location: index.html');
+        exit();
+    }
+}
+else
+{
+    $_SESSION['user']=[
+        "status"=>'false',
+    ];
+    header('Location: index.html');
+    exit();
+}
