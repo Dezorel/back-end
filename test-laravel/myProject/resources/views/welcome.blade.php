8b3d8c764ca3ba135cd5c8180976d5ca82e50bce
Leonid <scorpion23997@gmail.com>
2021-05-09 20:59:45 +0300

    Update welcome.blade.php

--- test-laravel/myProject/resources/views/welcome.blade.php
+++ test-laravel/myProject/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello this is my project Laravel
                 </div>
 
                 <div class="links">
